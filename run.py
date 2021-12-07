df00417f83dd608582a4763e9c33ed71a78263c7
JavierBL89 <notri80@gmail.com>
2021-12-07 22:40:56 +0000

    Access APIs

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
+
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.auth.service account import credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = credentials.from_google_service_account_file("creds.json")
+SCOPE_CREDS = CREDS.with_scope(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPE_CREDS)
+SHEET = GSPREAD_CLIENT.open("love_sandwitches")
