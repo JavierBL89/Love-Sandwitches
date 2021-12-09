b04e87d9d95ff20889e8d9becab33d2d87450564
JavierBL89 <notri80@gmail.com>
2021-12-09 11:21:32 +0000

    Validate data user entered function working

--- run.py
+++ run.py
@@ -14,12 +14,40 @@ SCOPE_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPE_CREDS)
 SHEET = GSPREAD_CLIENT.open("love_sandwitches")
 
+#WE DELETE THIS CODE AS IT IS ONLY TO CHECK THAT THE API WAS WORKING
+# #let's access the data in our sales worksheet
+# sales = SHEET.worksheet("sales") #we call th worksheet() method of the SHEET 
+#                                  # and pass it the sales worksheet
+# #we use the gpread method get_all_values to pull the value from the worksheet
+# data = sales.get_all_values()
 
-#let's access the data in our sales worksheet
-sales = SHEET.worksheet("sales") #we call th worksheet() method of the SHEET 
-                                 # and pass it the sales worksheet
+# print(data)
 
-#we use the gpread method get_all_values to pull the value from the worksheet
-data = sales.get_all_values()
+def get_data_sales_user():
+    """
+    Get sales input data from users
+    """
+    print("Please enter sales data from the last market")
+    print("Dta shoul be six numbers separated by comma.")
+    print("Example, 24,22,85,12,5,51\n")
 
-print(data)
+    data_str = input("Enter data: ")
+    values = data_str.split(",")
+    valid_data_sales(values)
+    
+
+def valid_data_sales(values):
+    """
+    Inside the try, converts all strings into integers.
+    Raises an error if the strings cnnot be converted 
+    or there aren't exactly 6 values
+    """
+    try:
+        [int(value) for value in values]
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 values required, you entered {len(values)}")
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again")
+
+get_data_sales_user()
